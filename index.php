99a9394e8c6c9f77dede05a25cf5d950162dabe1
Centeno Jose Antonio <jocecito182@gmail.com>
2021-10-19 09:11:28 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'assets/templates/header.php'; ?>
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+        <h1>Centeno José Antonio</h1>
+        <p>I'm a <span class="typed" data-typed-items="Full Stack Developer, Freelancer, Web Designer"></span></p>
+    </div>
+</section>
+<!-- End Hero -->
+
+<main id="main">
+    <?php include 'assets/templates/about.php'; ?>
+    <?php include 'assets/templates/resume.php'; ?>
+    <?php include 'assets/templates/portfolio.php'; ?>
+    <?php include 'assets/templates/contacto.php'; ?>
+</main>
+<!-- End #main -->
+<?php include 'assets/templates/footer.php'; ?>
