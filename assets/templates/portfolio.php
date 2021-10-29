e99778aebe17c683732cb45b7b6cc281128f0dc8
Centeno Jose Antonio <jocecito182@gmail.com>
2021-10-29 14:22:15 -0300

    comienzo portfolio

--- assets/templates/portfolio.php
+++ assets/templates/portfolio.php
@@ -14,27 +14,27 @@
 <div class="row">
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
         <div class="icon"><i class="bi bi-briefcase"></i></div>
-        <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-        <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+        <h4 class="title"><a href="">Bienes Raices</a></h4>
+        <p class="description">Encontrá la propiedad que querés en BienesRaices.</p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
         <div class="icon"><i class="bi bi-card-checklist"></i></div>
-        <h4 class="title"><a href="">Dolor Sitema</a></h4>
-        <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+        <h4 class="title"><a href="">App Salon</a></h4>
+        <p class="description">Aplicación para reservar turno en un salón de belleza.</p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
         <div class="icon"><i class="bi bi-bar-chart"></i></div>
-        <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
+        <h4 class="title"><a href="">Drum</a></h4>
         <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
         <div class="icon"><i class="bi bi-binoculars"></i></div>
-        <h4 class="title"><a href="">Magni Dolores</a></h4>
+        <h4 class="title"><a href="">The Dice Game</a></h4>
         <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
         <div class="icon"><i class="bi bi-brightness-high"></i></div>
-        <h4 class="title"><a href="">Nemo Enim</a></h4>
+        <h4 class="title"><a href="">Holbox Piscinas</a></h4>
         <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
     </div>
     <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
